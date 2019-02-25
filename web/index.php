1386cadcb0b05c5a584f9d83a14d8489ae4597fa
Javier Claros <jclaros@ultragrupo.com>
2019-02-25 13:02:44 -0400

    Configuracion para Heroku

--- web/index.php
+++ web/index.php
@@ -1,8 +1,11 @@
 <?php
 
+echo 'Mi Heroku PHP APP';
+/*
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
 if ($verify_token === 'abc123'){
   echo $challenge;
 }
+*/
