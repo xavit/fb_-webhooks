b42b21d7d516b191eaaa47a0e6326b149fa031e7
Javier Claros <jclaros@ultragrupo.com>
2019-02-25 13:04:49 -0400

    Webhook listo

--- web/index.php
+++ web/index.php
@@ -1,11 +1,10 @@
 <?php
 
-echo 'Mi Heroku PHP APP';
-/*
+//echo 'Mi Heroku PHP APP';
+
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 
 if ($verify_token === 'abc123'){
   echo $challenge;
 }
-*/
