e3dbb82ac69207ed27b923bf281d0a60028f5943
Javier Claros <javiclaros@gmail.com>
2019-03-06 14:42:08 -0400

    ultima modificacion del ejemplo de facebook

--- web/index.php
+++ web/index.php
@@ -9,3 +9,6 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'abc123'){
   echo $challenge;
 }
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
